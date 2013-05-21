e58f3b95541e03adafda31e6a6a22ec2cc7bda29
Omar <OmarBenitezE@live.com.mx>
2013-05-21 14:50:32 -0600

    Agregando la pagina de Contacto

--- Gemfile
+++ Gemfile
@@ -44,8 +44,9 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
 
+gem 'pg', '>= 0.12.2'
+  #gem 'sqlite3', '1.3.5'
 group :development, :test do
-  gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
 end
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App | " }
+
   describe "Static Pages" do
     it "Debe tener la palabra 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -9,9 +11,9 @@ describe "StaticPages" do
       page.should have_content('Sample App')
     end
 
-    it "Deben tener el titulo correcto" do
+    it "Deben tener el titulo 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "#{@base_title}Home")
     end
 
   end
@@ -22,6 +24,12 @@ describe "StaticPages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "Deben tener el titulo 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "#{@base_title}Help")
+    end
+
   end
 
   describe "About page" do
@@ -30,7 +38,24 @@ describe "StaticPages" do
       visit '/static_pages/about'
       page.should have_content('About Us')
     end
+
+    it "Deben tener el titulo 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "#{@base_title}About Us")
+    end
   end
 
+  describe "Contact page" do
+
+    it "Debe tener la palabra 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact Us')
+    end
+    it "Deben tener el titulo 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{@base_title}Contact Us")
+    end
+
+  end
 
 end
