741af590438bf3bf361b6e8a8ba212d3940b84d8
Macario De La Peña <mack.delapena@gmail.com>
2013-05-30 00:47:31 -0600

    Traduccion hecha

--- db/schema.rb
+++ db/schema.rb
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(:version => 20130528210806) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
